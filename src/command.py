90c7e9f8bd9c99177ac6fca6104433776601d3a4
dmurmann <david@thefoundation.de>
2011-02-24 01:30:31 +0100

    warn about missing frames; optional overwriting of existing files

--- src/command.py
+++ src/command.py
@@ -24,7 +24,11 @@ def get_input(name):
                 raise ValueError('invalid selection "%s"' % user_input)
         else:
             seq = keys[0]
-        with sequence.sequence_links(sequence.iterate_sequence(name, seq[0], seq[1], sequences[seq])) as links:
+        names = list(sequence.iterate_sequence(name, seq[0], seq[1], sequences[seq]))
+        missing_frames = int(sequences[seq][-1])-int(sequences[seq][0])+1 - len(names)
+        if missing_frames:
+            print >>sys.stderr, 'warning: sequence has %d missing frames' % missing_frames
+        with sequence.sequence_links(names) as links:
             directory = os.path.dirname(links[0])
             extension = os.path.splitext(links[0])[1]
             yield 'mf://' + os.path.join(directory, '*' + extension)
@@ -33,6 +37,8 @@ def get_input(name):
 
 def _main():
     parser = optparse.OptionParser("usage: %prog [options] input output")
+    parser.add_option("-f", "--force", dest="force", action="store_true",
+                      default=False, help="overwrite existing output file")
     options, args = parser.parse_args()
     if len(args) != 2:
         parser.error('one input and one output is required')
@@ -40,8 +46,8 @@ def _main():
     output_name = args[1]
     if not os.path.exists(input_name):
         parser.error('input "%s" does not exist' % input_name)
-    if os.path.exists(output_name):
-        parser.error('output "%s" already exists' % output_name)
+    if not options.force and os.path.exists(output_name):
+        parser.error('output "%s" already exists (force with -f)' % output_name)
 
     for input in get_input(input_name):
         asynproc.encode(input, output_name)
